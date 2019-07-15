4205110fec3ddb85b2bbfe0db824a8dd75799b40
Ikechukwu <iykekings36@gmail.com>
2019-07-15 14:27:47 +0100

    add delete account db helper and API endpoint

--- index.js
+++ index.js
@@ -7,6 +7,10 @@ const getAccountById = id =>
   db('accounts')
     .where({ id })
     .first();
+const deleteAccount = id =>
+  db('accounts')
+    .where({ id })
+    .del();
 
 server.get('/accounts', async (req, res) => {
   try {
@@ -20,11 +24,24 @@ server.get('/accounts/:id', async (req, res) => {
   const { id } = req.params;
   try {
     const account = await getAccountById(id);
-    res.status(200).json(account);
+    if (account) {
+      res.status(200).json(account);
+    } else {
+      res.status(404).json({ error: 'No account with that id' });
+    }
   } catch (error) {
     res
       .status(500)
       .json({ error: "Couldn't retrieve the account with that id" });
+  }
+});
+server.delete('/accounts/:id', async (req, res) => {
+  const { id } = req.params;
+  try {
+    await deleteAccount(id);
+    res.status(200).json({ message: 'Account deleted' });
+  } catch (error) {
+    res.status(500).json({ error: "Couldn't delete the account with that id" });
   }
 });
 
