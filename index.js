180ed7cf69ba3877bba3723e25cb2e399326d098
Ikechukwu <iykekings36@gmail.com>
2019-07-15 14:16:01 +0100

    Run seeds and add get all accounts endpoint

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
 const server = require('./server.js');
+const db = require('./data/dbConfig');
 
 const PORT = process.env.PORT || 4000;
+const getAccounts = () => db('accounts');
+
+server.get('/', async (req, res) => {
+  try {
+    const accounts = await getAccounts();
+    res.status(200).json(accounts);
+  } catch (error) {
+    res.status(500).json({ error: "Couldn't retrieve the accounts" });
+  }
+});
 
 server.listen(PORT, () => {
   console.log(`Listening on port ${PORT}...`);
