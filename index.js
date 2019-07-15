657aa7714f938f6c52c5e4ede7da8ac1166250e5
Ikechukwu <iykekings36@gmail.com>
2019-07-15 14:20:54 +0100

    add get single account

--- index.js
+++ index.js
@@ -3,13 +3,28 @@ const db = require('./data/dbConfig');
 
 const PORT = process.env.PORT || 4000;
 const getAccounts = () => db('accounts');
+const getAccountById = id =>
+  db('accounts')
+    .where({ id })
+    .first();
 
-server.get('/', async (req, res) => {
+server.get('/accounts', async (req, res) => {
   try {
     const accounts = await getAccounts();
     res.status(200).json(accounts);
   } catch (error) {
     res.status(500).json({ error: "Couldn't retrieve the accounts" });
+  }
+});
+server.get('/accounts/:id', async (req, res) => {
+  const { id } = req.params;
+  try {
+    const account = await getAccountById(id);
+    res.status(200).json(account);
+  } catch (error) {
+    res
+      .status(500)
+      .json({ error: "Couldn't retrieve the account with that id" });
   }
 });
 
